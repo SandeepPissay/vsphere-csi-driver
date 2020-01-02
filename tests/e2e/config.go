541581ad5b5f4f1e0835107ecddb4be1d130edc7
Shalini Bhaskara <sbhaskara@vmware.com>
2020-01-02 12:33:44 -0800

    Create test util for datastore picked from TargetvSANFileShareDatastoreURLs

--- tests/e2e/config.go
+++ tests/e2e/config.go
@@ -28,6 +28,8 @@ type e2eTestConfig struct {
 		InsecureFlag bool `gcfg:"insecure-flag"`
 		// Datacenter in which VMs are located.
 		Datacenters string `gcfg:"datacenters"`
+		// Target datastore urls for provisioning file volumes.
+		TargetvSANFileShareDatastoreURLs string `gcfg:"targetvSANFileShareDatastoreURLs"`
 	}
 }
 
